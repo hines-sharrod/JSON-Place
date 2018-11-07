03ac084e339405d894b797a4971b1ff2ffdb4af7
Sharrod Hines <sharrodhines@gmail.com>
2018-11-07 04:57:31 -0500

    Updates

---
+++ precache-manifest.b7a4c149a75107e4359ede22fe0bf456.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "880b22fd9096bd2e601d374f44091114",
+    "url": "/JSON-Place/static/media/json-place.880b22fd.jpeg"
+  },
+  {
+    "revision": "415c4e22063e49e0483be5e2fba77a76",
+    "url": "/JSON-Place/static/media/jp-logo.415c4e22.png"
+  },
+  {
+    "revision": "58f45103be031ac17128",
+    "url": "/JSON-Place/static/js/runtime~main.58f45103.js"
+  },
+  {
+    "revision": "5957252838ce7c0688d3",
+    "url": "/JSON-Place/static/js/main.59572528.chunk.js"
+  },
+  {
+    "revision": "3bd301b84e1de808a11a",
+    "url": "/JSON-Place/static/js/1.3bd301b8.chunk.js"
+  },
+  {
+    "revision": "5957252838ce7c0688d3",
+    "url": "/JSON-Place/static/css/main.090d24ad.chunk.css"
+  },
+  {
+    "revision": "f963fe05a7111f70793a68efad0acfb3",
+    "url": "/JSON-Place/index.html"
+  }
+];
