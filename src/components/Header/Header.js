2a0b845ec4ce18ac7402c0f5ebe1cf166bcefac2
Sharrod Hines <sharrodhines@gmail.com>
2018-11-06 19:37:25 -0500

    initial app setup

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../css/App.css';
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="row">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header; 
