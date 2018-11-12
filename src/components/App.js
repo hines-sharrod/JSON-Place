081bd93eb6152d748689be589bd4d6b84d64185e
Sharrod Hines <sharrodhines@gmail.com>
2018-11-12 16:02:40 -0500

    initial project setup with Sassy React Starter

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+    <div className="App">
+        <h1>The SASS React Starter is Live!</h1>
+    </div>
+)
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      // Where to find the HTML file to inject the bundle into
+      template: "./src/index.html",
+      // The filename is the name the dist folder will name the outputted file
+      filename: "./index.html"
+    })
+  ]
+};
