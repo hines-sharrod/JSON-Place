bdbe2ee07d128c31b62ff664132b9a9167deec3b
Hines <0910203840@FEMA.DHS.GOV>
2018-11-13 13:34:53 -0500

    added in the skeleton css framework, babel class property plugin, and the select user element & function

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import '../assets/skeleton.scss';
 
-const App = () => (
-    <div className="App">
-        <h1>The SASS React Starter is Live!</h1>
-    </div>
-)
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 'defaultUser'
+        };
+    }
+
+    selectChangeHandler = e => {
+        this.setState({value: e.target.value})
+    }
+    
+    render() {
+        return (
+            <div className="App">
+                <div className="row">
+                    <select value={this.state.value} onChange={this.selectChangeHandler}>
+                        <option disabled value="defaultUser">Select a User</option>
+                        <option value="user1">User1</option>
+                    </select>
+                </div>
+                <div className="row">
+                    <div class="one-half column">1/2</div> 
+                    <div class="one-half column">1/2</div>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default App;
